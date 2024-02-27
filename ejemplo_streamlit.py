971ab3e3057ffa484ad75bd9bdc471a0ee6be354
pablogcortez <84234416+pablogcortez@users.noreply.github.com>
2024-02-27 14:10:05 -0300

    Update ejemplo_streamlit.py

--- ejemplo_streamlit.py
+++ ejemplo_streamlit.py
@@ -1,13 +1,19 @@
+import streamlit as st
+import pandas as pd
 
-st.title("Cargar archivo en Streamlit")
+def main():
+    st.title("Cargar archivo en Streamlit")
 
-# Widget para cargar el archivo
-uploaded_file = st.file_uploader("Cargar archivo", type=["csv"])
+    # Widget para cargar el archivo
+    uploaded_file = st.file_uploader("Cargar archivo", type=["csv"])
 
-if uploaded_file is not None:
-    # Leer el archivo en un DataFrame de Pandas
-    df = pd.read_csv(uploaded_file)
-    
-    # Mostrar el DataFrame
-    st.write("Contenido del archivo:")
-    st.write(df)
+    if uploaded_file is not None:
+        # Leer el archivo en un DataFrame de Pandas
+        df = pd.read_csv(uploaded_file)
+        
+        # Mostrar el DataFrame
+        st.write("Contenido del archivo:")
+        st.write(df)
+
+if __name__ == "__main__":
+    main()
