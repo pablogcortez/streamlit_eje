ac7760d47860e635e6b79bb6cdb56c7ed5e96f47
pablogcortez <84234416+pablogcortez@users.noreply.github.com>
2024-02-27 13:52:00 -0300

    ejemplo_streamlit.py

--- ejemplo_streamlit.py
+++ ejemplo_streamlit.py
@@ -2,17 +2,17 @@ import streamlit as st
 import pandas as pd
 
 def main():
-    st.title("Cargar archivo de Excel en Streamlit")
+    st.title("Cargar archivo en Streamlit")
 
-    # Widget para cargar el archivo de Excel
-    uploaded_file = st.file_uploader("Cargar archivo Excel", type=["xlsx"])
+    # Widget para cargar el archivo
+    uploaded_file = st.file_uploader("Cargar archivo", type=["csv"])
 
     if uploaded_file is not None:
-        # Leer el archivo de Excel en un DataFrame de Pandas
-        df = pd.read_excel(uploaded_file)
+        # Leer el archivo en un DataFrame de Pandas
+        df = pd.read_csv(uploaded_file)
         
         # Mostrar el DataFrame
-        st.write("Contenido del archivo Excel:")
+        st.write("Contenido del archivo:")
         st.write(df)
 
 if __name__ == "__main__":
